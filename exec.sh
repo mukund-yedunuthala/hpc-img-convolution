61f2fdc79534f78ec1394036c55a9dc13430c059
Venkata Mukund Kashyap Yedunuthala <mukundkashyapyn@gmail.com>
2021-02-15 22:58:32 +0100

    add/update pngs, change scriptfile to include report

--- exec.sh
+++ exec.sh
@@ -2,9 +2,16 @@
 g++ -o main.out main.cpp &&
 ./main.out &&
 echo "Converting 1" &&
-convert result01-edgedetection.pgm result01-edgedetection.jpg &&
+convert result01-edgedetection.pgm result01-edgedetection.png &&
 echo "Converting 2" &&
-convert result02-blur.pgm result02-blur.jpg &&
+convert result02-blur.pgm result02-blur.png &&
 echo "Converting 3" &&
-convert result03-all.pgm result03-all.jpg &&
-echo "Job finished"
+convert result03-all.pgm result03-all.png &&
+echo "Job finished" &&
+echo "Generating updated report" &&
+if command -v pdflatex &> /dev/null
+then
+    pdflatex Documentation.tex
+    echo "Report updated"
+    exit
+fi
