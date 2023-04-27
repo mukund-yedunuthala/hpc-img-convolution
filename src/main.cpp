45e45d5b42f4a4d953eec3a12a8f742ced9f6baa
Venkata Mukund Kashyap Yedunuthala <mukundkash1997@gmx.net>
2023-04-27 19:13:40 +0200

    enable optional mpi configuration

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,13 @@
 
 #include "io.h"
 #include "helper.h"
-#include "convolutions.h"
 #include "HPC2020Config.h"
+#ifdef WITH_MPI
+    #include "mpi_convolutions.h"
+#else
+    #include <time.h>
+    #include "convolutions.h"
+#endif
 /**
  * \brief Main function.
  * 
@@ -26,7 +31,6 @@ int main(int argc, char** argv) {
     unsigned short int maxVal{};
     unsigned short int matSize{3};
     unsigned short int blurInstances{5}, edgeInstances{1}, sharpInstances{1};
-    double wallTime{};
     std::string ipFileName{""}, opFileName{""};
     ipFileName = "../inputs/512.pgm";
 
@@ -61,47 +65,80 @@ int main(int argc, char** argv) {
     }
     // std::cout << "Edge address (out): " << &edgeDetection << std::endl;
     set_kernels(edgeDetection, identity, sharpen, gaussBlur);
+#ifdef WITH_MPI
+    double wallTime{};
     MPI_Init(&argc, &argv);
     wallTime = MPI_Wtime();
     MPI_Comm_size(MPI_COMM_WORLD, &WORLD_SIZE);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
+#else
+    clock_t wallTime = clock();
+#endif
+
     set_array(valueArray, arrSize, inputArray);
     set_array(resultArray, arrSize, defaultValue);
+    opFileName = "../output/512edge.pgm";
+#ifdef WITH_MPI
     setup_convolution<int>(
             numRows, numCols, valueArray, edgeDetection,
             resultArray, matSize, edgeInstances 
         );
-    opFileName = "../output/512edge.pgm";
     if (rank == ROOT) { write_to_file(resultArray, numRows, numCols, maxVal, opFileName); }
+#else
+    convolution<int>(
+        valueArray, resultArray, edgeDetection, 
+        numRows, numCols, matSize, edgeInstances
+    );
+    write_to_file(resultArray, numRows, numCols, maxVal, opFileName);
+#endif
     
     set_array(valueArray, arrSize, inputArray);
     set_array(resultArray, arrSize, defaultValue);
+    opFileName = "../output/512sharpen.pgm";
+#ifdef WITH_MPI
     setup_convolution<int>(
             numRows, numCols, valueArray, sharpen,
             resultArray, matSize, sharpInstances 
         );
-    opFileName = "../output/512sharpen.pgm";
     if (rank == ROOT) { write_to_file(resultArray, numRows, numCols, maxVal, opFileName); }
-    
+#else
+    convolution<int>(
+        valueArray, resultArray, sharpen, 
+        numRows, numCols, matSize, sharpInstances
+    );
+    write_to_file(resultArray, numRows, numCols, maxVal, opFileName);
+#endif
+
     set_array(valueArray, arrSize, inputArray);
     set_array(resultArray, arrSize, defaultValue);
+    opFileName = "../output/512blur.pgm";
+#ifdef WITH_MPI
     setup_convolution<double>(
             numRows, numCols, valueArray, gaussBlur,
             resultArray, matSize, blurInstances 
         );
-    opFileName = "../output/512blur.pgm";
     if (rank == ROOT) { write_to_file(resultArray, numRows, numCols, maxVal, opFileName); }
+#else
+    convolution<double>(
+        valueArray, resultArray, gaussBlur, 
+        numRows, numCols, matSize, blurInstances
+    );
+    write_to_file(resultArray, numRows, numCols, maxVal, opFileName);
+#endif
     
+#ifdef WITH_MPI
     if (rank == ROOT) {
         printinfo(numRows, numCols, maxVal);
         wallTime = MPI_Wtime() - wallTime;
         print_time_info(wallTime);
     }
-
     MPI_Finalize();
-    
-    
+#else
+    wallTime = clock() - wallTime;
+    printinfo(numRows, numCols, maxVal);
+    double elapsed = (double) wallTime/CLOCKS_PER_SEC;
+    print_time_info(elapsed);
+#endif
     for (size_t i = 0; i < matSize; i++){
         delete[] edgeDetection[i];
         delete[] identity[i];
@@ -124,10 +161,6 @@ int main(int argc, char** argv) {
     resultArray = nullptr;
     delete[] inputArray;
     inputArray = nullptr;
-
-#ifdef WITH_OPTION
-    std::cout << "This worked.\n";
-#endif
 
     return 0;
 }
